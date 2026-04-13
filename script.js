94ff6479c54df29a399cd1d645db0abff38a4bd1
Scott Purcival <scottpurcival@MacBook-Pro.local>
2026-04-13 19:56:16 +1000

    Added Gallery

--- script.js
+++ script.js
@@ -23,3 +23,38 @@ function scrambleIt(b,a,css,text){
     var c = ".com";
     document.write('<a href="mailto:'+a+'@'+b+c+'" class="'+css+'">'+text+'</a>');
 }
+
+var galleryImages = [
+    'images/gallery/haircut-1.jpg',
+    'images/gallery/haircut-2.jpg',
+    'images/gallery/haircut-3.jpg',
+    'images/gallery/markets-1.jpg',
+    'images/gallery/markets-2.jpg',
+    'images/gallery/salon.jpg'
+];
+var galleryIndex = 0;
+
+function openGallery(index) {
+    galleryIndex = index;
+    document.getElementById('gallery-modal-img').src = galleryImages[galleryIndex];
+    document.getElementById('gallery-counter').textContent = (galleryIndex + 1) + ' / ' + galleryImages.length;
+    document.getElementById('gallery-modal').classList.add('open');
+    document.addEventListener('keydown', galleryKeyHandler);
+}
+
+function closeGallery() {
+    document.getElementById('gallery-modal').classList.remove('open');
+    document.removeEventListener('keydown', galleryKeyHandler);
+}
+
+function changeGalleryImage(dir) {
+    galleryIndex = (galleryIndex + dir + galleryImages.length) % galleryImages.length;
+    document.getElementById('gallery-modal-img').src = galleryImages[galleryIndex];
+    document.getElementById('gallery-counter').textContent = (galleryIndex + 1) + ' / ' + galleryImages.length;
+}
+
+function galleryKeyHandler(e) {
+    if (e.key === 'Escape') closeGallery();
+    if (e.key === 'ArrowLeft') changeGalleryImage(-1);
+    if (e.key === 'ArrowRight') changeGalleryImage(1);
+}
